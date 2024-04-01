815bcf1ed14bccde8d5e056507d52a33a12eea43
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-04-01 16:35:42 +0300

    snackbar without deley

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,5 +3,39 @@ import iziToast from "izitoast";
 
 import "izitoast/dist/css/iziToast.min.css";
 
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", handleCreate);
+ 
+const delay = form.elements.delay.value;
+console.dir(delay);
+
+function handleCreate(event) {
+    event.preventDefault();
+    const promises = (() => {
+        return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (form.elements.state.value === "fulfilled") {
+                resolve(`✅ Fulfilled promise in ${delay}ms`)
+            } else {
+                reject(`❌ Rejected promise in ${delay}ms`)
+            }
+        }, delay);
+        
+    })
+    })
+    promises()
+        .then(value => iziToast.info({
+            message: value
+        }))
+        .catch(error => iziToast.info({
+            message: error
+        })); 
+    
+     
+};
+
+
+
 
 
